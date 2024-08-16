dd0f42367b79165b0d828f1151050101f35e5285
unknown <benjajuarez@gmail.com>
2024-08-16 17:36:12 -0300

    Haciendo el front con react

---
+++ src/components/FooterC.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FooterC = () => {
+  return (
+    <footer className='py-5-text-center bg-body-tertiary'>
+      <h3>footer</h3>
+    </footer>
+  )
+}
+
+export default FooterC
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import CardsC from "../components/CardsC"
+import CarrouselC from "../components/CarrouselC"
+import FooterC from "../components/FooterC"
+import NavbarC from "../components/NavbarC"
+
+ 
+
+const HomePage = () => {
+  return (
+    <>
+      <NavbarC />
+      <CarrouselC />
+      <CardsC />
+      <FooterC />
+    </>
+  )
+}
+
+export default HomePage
