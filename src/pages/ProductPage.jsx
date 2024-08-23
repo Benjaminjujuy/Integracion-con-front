10bd8ad118a64c636d87a30ab8a8c2e5ba6c8a62
unknown <benjajuarez@gmail.com>
2024-08-23 09:14:35 -0300

    termine los botones de agregar a carrito y fav

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -7,9 +7,30 @@ import { Button } from "react-bootstrap"
 const ProductPage = () => {
     const params = useParams()
     const [product, setProducts] = useState({})
+
     const obtenerProducto = async() => {
     const result = await clienteAxios.get(`/productos/${params.idProducto}`)
     setProducts(result.data.producto)
+    }
+
+    const handleAddProdFav = async() => {
+      const token = JSON.parse(sessionStorage.getItem('token'))
+      const result = await clienteAxios.post(`productos/agregarProdFav/${product._id}`, {}, {
+        headers: {
+          "Content-Type": 'application/json',
+          'auth': `${token}`
+        }
+      })
+    }
+
+    const handleAddProdCart = async() => {
+      const token = JSON.parse(sessionStorage.getItem('token'))
+      const result = await clienteAxios.post(`productos/agregarProdCart/${product._id}`, {}, {
+        headers: {
+          "Content-Type": 'application/json',
+          'auth': `${token}`
+        }
+      })
     }
 
     useEffect(() => {
